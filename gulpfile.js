d9c8641f328b183fa511d5c0323422f45347c1db
Geekatori <tups590@gmail.com>
2015-11-30 14:13:37 +0100

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnext = require('cssnext');
+var precss = require('precss');
+var cssnano = require('cssnano');
+
+gulp.task('css', function () {
+  var processors = [
+    autoprefixer,
+    cssnext,
+    precss,
+    cssnano
+  ];
+  return gulp.src('./src/*.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./dest'));
+});
